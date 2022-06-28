a7b983e9dfcedf5af7bc7635d860cb204d54a6a7
Agustin <aste.agustin@gmail.com>
2022-06-28 15:05:57 -0300

    2 commit for date 28-6-2022 15:6:8

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const nodeCron = require("node-cron");
 const fs = require("fs");
 const moment = require("moment");
+const simpleGit = require('simple-git');
+
+
+
+
+
+
+
+
 
 const makeChangesToFile = (content) => {
     console.log("content", content)
@@ -14,22 +23,30 @@ const makeChangesToFile = (content) => {
     })
 
 }
-const pushChangesToGit = () => {
-    await git.cwd({ path: 'content' }).commit('test commit');
+const pushChangesToGit = async(msg) => {
+    console.log("committing on git");
+    simpleGit()
+        .init()
+        .add('./*')
+        .commit(msg)
+        .addRemote('origin', 'https://github.com/AgustinAste/auto-py-commits.git')
+        .push('origin', 'master');
+    console.log("ended committing on git");
 
 }
 
 // will run the task at 1:05:30pm every day
-const job = nodeCron.schedule("30 5 13 * * *", function jobYouNeedToExecute() {
-    // Do whatever you want in here. Send email, Make  database backup or download data.
-    let ammount_of_commits = Math.floor(Math.random() * 10);
-    console.log('ammount_of_commits', ammount_of_commits)
-    for (let i = 0; i < ammount_of_commits; i++) {
-        let date_now = moment().format('D-M-Y H:M:S')
+// const job = nodeCron.schedule("30 5 13 * * *", function jobYouNeedToExecute() {
+// Do whatever you want in here. Send email, Make  database backup or download data.
+let ammount_of_commits = Math.floor(Math.random() * 10);
+console.log('ammount_of_commits', ammount_of_commits)
+for (let i = 0; i < ammount_of_commits; i++) {
+    let date_now = moment().format('D-M-Y H:M:S')
 
-        let file_content = '\n' + date_now + " " + i;
-        makeChangesToFile(file_content);
-        pushChangesToGit();
+    let file_content = '\n' + date_now + " " + i;
+    makeChangesToFile(file_content);
 
-    }
-});
+    pushChangesToGit(i + ' commit for date ' + date_now);
+
+}
+// });
